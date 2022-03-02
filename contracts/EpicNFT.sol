6b9fc1d69e4fdb9a3e631455a0ab1a0e2830027a
tommaay <tommy@tomster.dev>
2022-03-01 19:00:38 -0800

    Adding old cold to new repo

---
+++ contracts/EpicNFT.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.1;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+
+import {Base64} from "./libraries/Base64.sol";
+
+contract EpicNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("SquareNFT", "SQUARE") {
+        console.log("This is the Buildspace NFT project contract!");
+    }
+
+    event NewEpicNFTMinted(address sender, uint256 tokenId);
+
+    function makeAnEpicNFT() public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
+
+        // Actually mint the NFT to the sender using msg.sender.
+        _safeMint(msg.sender, newItemId);
+
+        // Set the NFTs data.
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/9RHP");
+        console.log(
+            "An NFT w/ ID %s has been minted to %s",
+            newItemId,
+            msg.sender
+        );
+
+        emit NewEpicNFTMinted(msg.sender, newItemId);
+
+        // Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
+    }
+}
